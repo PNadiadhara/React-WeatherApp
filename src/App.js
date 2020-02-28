70b220ab1d79faf8cd560af90c998de12f1f11d3
PN <priteshrna@gmail.com>
2020-02-27 22:21:53 -0500

    Date displays correctly

--- src/App.js
+++ src/App.js
@@ -6,16 +6,34 @@ const api = {
 }
 
 function App() {
+  // using native DateObject
+const dateBuilder = (d) => {
+  let months = ["January","February","March","April","May","June","July",
+  "August","September","October","November","December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+  let day = days[d.getDay()]; // returns 0 .. 6
+  let date = d.getDate(); // returns date 1 .. 31 
+  let month = months[d.getMonth()] // returns 0 .. 11
+  let year = d.getFullYear();
+
+  
+
+  return `${day} ${date} ${month} ${year}`
+}
+
   return (
-    <div className="App">
+    <div className="App warm">
       <main>
         <div className="search-box">
           <input type="text"
           className="search-bar"
           placeholder="Search..."
-          /> 
-          
-          
+          />           
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div> 
         </div>
       </main>
     </div>
