7f5497a20614bc3e3c60263992eac44068774ecd
PN <priteshrna@gmail.com>
2020-02-27 21:22:24 -0500

    search bar implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+const api = {
+  key: "50bb2091655ad1fec8ff77fa8a9b54ae",
+  base: "http://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      Hello
-      tested
+      <main>
+        <div className="search-box">
+          <input type="text"
+          className="search-bar"
+          placeholder="Search..."
+          /> 
+          
+          
+        </div>
+      </main>
     </div>
   );
 }
